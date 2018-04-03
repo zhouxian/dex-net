0e74d4f07ee5f30397f1a12eff1ec804f64170a9
Zhou Xian <zhouxian.sq@gmail.com>
2018-04-03 15:09:02 -0400

    Edit install.sh to replace berkeley's meshpy with zhouxian/meshpy_berkeley to resolve conflict between two meshpy packages

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ esac
 sudo apt-get install cmake libvtk5-dev python-vtk python-sip python-qt4 libosmesa6-dev meshlab libhdf5-dev
 
 # install pip deps
-pip install numpy scipy scikit-learn scikit-image opencv-python pyassimp tensorflow h5py mayavi matplotlib catkin_pkg multiprocess dill cvxopt ipython pillow pyhull setproctitle trimesh
+pip install numpy scipy scikit-learn scikit-image opencv-python pyassimp tensorflow h5py mayavi matplotlib catkin_pkg multiprocess dill cvxopt ipython pillow pyhull setproctitle trimesh meshpy
 
 # install deps from source
 mkdir deps
@@ -64,11 +64,11 @@ cd ${MODULES_DIR}
 git clone https://github.com/BerkeleyAutomation/autolab_core.git
 git clone https://github.com/BerkeleyAutomation/perception.git
 git clone https://github.com/BerkeleyAutomation/gqcnn.git
-git clone https://github.com/BerkeleyAutomation/meshpy.git
+git clone https://github.com/zhouxian/meshpy_berkeley.git
 git clone https://github.com/BerkeleyAutomation/visualization.git
 
-# install meshpy
-cd meshpy
+# install meshpy_berkeley
+cd meshpy_berkeley
 python setup.py develop
 cd ../
 
